d456932b40ab5354113bc5582a9194eefb28ec25
harish-ragavendra-25 <hragavendra25@gmail.com>
2024-07-14 10:03:35 +0530

    sessions info added to DB

--- app.js
+++ app.js
@@ -12,17 +12,22 @@ const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-const session = require('express-session');
 const flash = require('connect-flash');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const ExpressError = require('./utilities/ExpressError');
 const userModel = require('./models/user');
+// const DB_URL = process.env.DB_URL;
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 
-mongoose.connect("mongodb://localhost:27017/wild-whirl")
+const DB_URL = "mongodb://localhost:27017/wild-whirl";
+
+mongoose.connect(DB_URL)
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
@@ -93,9 +98,20 @@ app.use(
   })
 )
 
+const store = MongoStore.create({
+  mongoUrl: DB_URL,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+    secret: 'thisshouldbeabettersecret!'
+  }
+});
+
+store.on("error",function(e){
+  console.log("Store Error",e);
+})
 
 const sessionConfig = {
-  name:'session',
+  store,
   secret: "thisshouldbebettersecret",
   resave: false,
   saveUninitialized: true,
@@ -122,7 +138,6 @@ const userRoutes = require('./routes/userRoutes');
 const { connect } = require('http2');
 
 app.use((req,res,next) => {
-  console.log(process.env.NODE_ENV);
   res.locals.currentUser = req.user;
   res.locals.sucess = req.flash('sucess');
   res.locals.error = req.flash('error');
